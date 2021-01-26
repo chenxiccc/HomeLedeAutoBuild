cd408ce168d0dbd0851603f0f9b99ca6b6ed3225
chenxiccc <chenxiccc@gmail.com>
2021-01-26 20:59:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,11 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
 sed -i 's/192.168.1.1/192.168.31.200/g' package/base-files/files/bin/config_generate
 wget -O package/base-files/files/etc/hosts https://raw.githubusercontent.com/chenxiccc/Customfiles/master/hosts
+wget -O tmp/dhcp.tmp https://raw.githubusercontent.com/chenxiccc/Customfiles/master/newifi3/dhcp
+cat tmp/dhcp.tmp >> package/network/services/dnsmasq/files/dhcp.conf
+rm tmp/dhcp.tmp
+wget -O tmp/firewall.tmp https://raw.githubusercontent.com/chenxiccc/Customfiles/master/newifi3/firewall
+cat tmp/firewall.tmp >> package/network/config/firewall/files/firewall.config
+rm tmp/firewall.tmp
